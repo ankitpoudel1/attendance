c0c4fb3c7ba91ef83d0627bb9537daa60a6c054f
ankit <ankitpoudel1@gmail.com>
2024-05-26 14:20:52 +0545

    leave request post

--- server/routes/leaveRequest.js
+++ server/routes/leaveRequest.js
@@ -38,3 +38,49 @@ router.get(
   
   }
 );
+
+router.post(
+    "/",
+    [
+      body("employeeId", "Employee Id is required").not().isEmpty(),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+  
+      const { employeeId, remarks } = req.body;
+  
+  
+      try {
+        var user = await User.findOne({ employeeId });
+  
+        if (!user) {
+          return res
+            .status(400)
+            .json({ errors: [{ msg: "User not found" }] });
+        }
+  
+        var leaveRequest = new LeaveRequest({
+          employee:user.id,
+          employeeName:user.name,
+          remarks
+        })
+  
+        await leaveRequest.save();
+     
+  
+        res.status(200).send("Leave request recorded successfully");
+  
+      } catch (error) {
+        console.error(error.message);
+        // res.status(500).send('Server error');
+        res.status(500).send(error.message);
+      }
+    }
+  );
+
+
+module.exports = router;
